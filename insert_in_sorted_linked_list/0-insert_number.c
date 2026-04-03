ebfe002f239f52143e1eb942c66cebf7c0300cec
THO-Gaetan <9771@holbertonstudents.com>
2026-04-03 11:49:47 +0200

    adding the readme

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * insert_node - insert an integer in a single linked list
+ * @head: pointer to the head of the list
+ * @number: the integer who need to be insert in the list
+ *
+ * Description: This program is inserting a number in
+ * a single linked list
+ *
+ * Return: return head
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *new;
---
+++ insert_in_sorted_linked_list/README.md
@@ -0,0 +1,1 @@
+Insert in sorted linked list - algo project - C27
