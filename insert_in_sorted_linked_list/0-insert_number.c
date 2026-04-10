c79c690bb25a2548b1ef83d4b342e96bde095047
THO-Gaetan <9771@holbertonstudents.com>
2026-04-10 09:43:26 +0200

    adjusting the code so it place the new node at the right spot

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -3,28 +3,31 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * insert_node - insert an integer in a single linked list
- * @head: pointer to the head of the list
- * @number: the integer who need to be insert in the list
- *
- * Description: This program is inserting a number in
- * a single linked list
- *
- * Return: return head
- */
+* insert_node - insert an integer in a single linked list
+* @head: pointer to the head of the list
+* @number: the integer who need to be insert in the list
+*
+* Description: This program is inserting a number in
+* a single linked list
+*
+* Return: return head
+*/
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new;
+	listint_t *new;
 
-    new = malloc(sizeof(listint_t));
-    
-    if (new == NULL)
-    {
-        printf("Error");
-        return (NULL);
-    }
-    new->n = number;
-    new->next = *head;
-    *head = new;
-    return (*head);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		printf("Error");
+		return (NULL);
+	}
+	while (*head != NULL && (*head)->n < number)
+	{
+		head = &(*head)->next;
+	}
+	new->n = number;
+	new->next = *head;
+	*head = new;
+	return (*head);
 }
